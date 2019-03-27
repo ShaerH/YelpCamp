df19a55dc58d04da319b93a52da5eab20d668628
Shaer Hasan <shaer2m@gmail.com>
2019-03-27 23:11:32 +0000

    Added form to add more campgrounds

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-#YelpCamp
-
+# 1. YelpCamp
 * Add Landing Page
 * Add Campgrounds Page that lists all campgrounds
 
-Each Campground has:
+# 2. Each Campground has:
 * Name
 * Image
 
-Layout and Basic Styling
+# 3. Layout and Basic Styling
 * Create our header and footer partials
 * Add in Bootstrap
+
+# 4. Creating New Campgrounds
+* Setup new Campgrounds POST route
+* Add in body-parser
+* Setup route to show form
+* Add basic unstyled form
+
+# 5. Style the campgrounds page
+* Add a better Header/title
+* Make Campgrounds diplay in a grid
--- app.js
+++ app.js
@@ -1,27 +1,50 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
-
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
     res.render("landing");
-})
+});
 
-app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
+var campgrounds = [
         {name: "Salmon Creek", image: "https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
         {name: "Montain Goats", image: "http://www.road-trip-usa.com/uploads/6/1/9/4/61940309/ventana-campground-campgrounds-in-big-sur_orig.jpg"},
         {name: "Granite Hill", image: "https://dailygazette.com/sites/default/files/styles/article_image/public/180702d.jpg?itok=6L_qDMLP"}
         ]
+        
+// show all campgrounds
+app.get("/campgrounds", function(req, res) {
+    
    res.render("campgrounds",{campgrounds: campgrounds}); 
+});
+
+//create a new campground
+app.post("/campgrounds", function(req, res){
+    // get data from form and add to campground array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name:name, image:image};
+    campgrounds.push(newCampground);
+    
+    // redirect back to campgrounds page
+    res.redirect("/campgrounds");
+
+});
+
+//show the form that will send the data to post route above
+app.get("/campgrounds/new", function(req, res) {
+
+   res.render("new.ejs");
+
 });
 
 app.get("*", function(req, res){
     res.send("Error page");
 });
-
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp app has started!!");
 });
