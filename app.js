846935437cc46fedd48bef382a26bd5ca697afa3
Shaer Hasan <shaer2m@gmail.com>
2019-03-28 01:41:54 +0000

    navbar added

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ app.set('view engine', 'ejs');
 app.get("/", function(req, res) {
     res.render("landing");
 });
-
 var campgrounds = [
         {name: "Salmon Creek", image: "https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
         {name: "Montain Goats", image: "http://www.road-trip-usa.com/uploads/6/1/9/4/61940309/ventana-campground-campgrounds-in-big-sur_orig.jpg"},
         {name: "Granite Hill", image: "https://dailygazette.com/sites/default/files/styles/article_image/public/180702d.jpg?itok=6L_qDMLP"}
         ]
-        
 // show all campgrounds
 app.get("/campgrounds", function(req, res) {
     
